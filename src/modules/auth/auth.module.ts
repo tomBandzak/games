04e6e8a3a5250629452a10bf368a98cc9b47863b
RD_dev <TzJBKTvQ7cTwuu8T>
2019-04-11 19:41:11 +0200

    -- user module added -- DB used for authentication

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from '../user/user.module';
 import { AuthService } from './auth.service';
 import { HttpStrategy } from './http.strategy';
+import { UserService } from '../user/user.service';
 
 @Module({
-  providers: [AuthService, HttpStrategy],
+  imports: [UserModule],
+  providers: [
+    AuthService,
+    HttpStrategy,
+    UserService,
+  ],
 })
 export class AuthModule{}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class AuthService {
-  constructor() {}
+  constructor(
+    private readonly userService: UserService,
+  ) {}
 
   async validateUser(token: string): Promise<any> {
-    return token === 'TOKEN';
+    return await this.userService.getOneByToken(token);
   }
 }
